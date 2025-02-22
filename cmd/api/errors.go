1d420502cfb8b68cbe662612f71c3a90b85ff7d7
brainwhat <facelessbeach@gmail.com>
2025-02-22 17:58:45 +0300

    typo fix

--- cmd/api/errors.go
+++ cmd/api/errors.go
@@ -50,7 +50,7 @@ func (app *application) failedValidationResponse(w http.ResponseWriter, r *http.
 	app.errorResponse(w, r, http.StatusUnprocessableEntity, errors)
 }
 
-func (app *application) editConflictRecspnse(w http.ResponseWriter, r *http.Request) {
+func (app *application) editConflictResponse(w http.ResponseWriter, r *http.Request) {
 	message := "unable to update record due to an edit conflict, try again"
 	app.errorResponse(w, r, http.StatusConflict, message)
 }
